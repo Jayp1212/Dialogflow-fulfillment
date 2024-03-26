1645d5275b9cf842ac969b0c1586ffd1ba76bf14
Jayp1212 <200543276@student.georgianc.on.ca>
2024-03-25 20:30:38 -0400

    app commit

--- app.py
+++ app.py
@@ -7,18 +7,21 @@ app = Flask(__name__)
 def student_number():
     return jsonify({'student_number': 'YourStudentNumber'})
 
-# Route to handle webhook requests from Dialogflow
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    # Process the request from Dialogflow
-    # Extract intent and parameters, perform any necessary logic
-    # Generate response text
-    # Return response to Dialogflow
-    response = {
-        'fulfillmentText': 'This is the response from your webhook.'
-    }
-    return jsonify(response)
+    fulfillmentText = ''
+    
+    if req:
+        query_result = req.get('queryResult')
+        if query_result and query_result.get('action') == 'get.address':
+            fulfillmentText = 'Hi'
+
+    return jsonify({
+        "fulfillmentText": fulfillmentText,
+        "source": "webhookdata"
+    })
+
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Run the Flask app
+    app.run(debug=True)  
